45177169e91d60994dfdaa96b9cea2ac498e1cc5
Saul Belisle <rbelisle1270@gmail.com>
2021-05-09 12:07:11 -0700

    react boilerplate

--- task-app/src/app.js
+++ task-app/src/app.js
@@ -1,5 +1,16 @@
+import React from 'react';
+import Overview from './components/Overview';
+
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div>
+			<form>
+				<input type="text"></input>
+				<button type="button">Submit!</button>
+			</form>
+			<Overview />
+		</div>
+	);
 }
 
 export default App;
---
+++ task-app/src/components/Overview.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Overview() {
+	let tasks = ['A', 'B', 'C'];
+	console.log(tasks);
+
+	return (
+		<div>
+			<ul></ul>
+		</div>
+	);
+}
+
+export default Overview;
